59e28fb80f174dc8327f74c3563d551e9a2b85e7
Kunal Arora <kunalarora.135@gmail.com>
2018-10-21 15:39:00 +0530

    requirements and dependencies

---
+++ requirements/pleInstall.py
@@ -0,0 +1,6 @@
+import os
+!git clone https://github.com/ntasfi/PyGame-Learning-Environment.git
+os.chdir('PyGame-Learning-Environment')
+!pip install -e .
+!pip install pygame
+os.chdir('/content')
