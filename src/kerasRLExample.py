14245c68eb4bc7ebe8d4bdcaaf08dc5692927f60
Kunal Arora <kunalarora.135@gmail.com>
2018-11-06 10:19:25 +0530

    cleanup

--- src/kerasRLExample.py
+++ src/kerasRLExample.py
@@ -19,128 +19,149 @@ from PIL import Image
 SCALED_IMAGE_HEIGHT=80  
 SCALED_IMAGE_WIDTH=80
 WINDOW_LENGTH=4
-TRAINING_STEPS=200
+TRAINING_STEPS=500000
+TEST_EPISODES=5
+LOAD=False
+LOAD_FILE='dqn_FlappyBird-v0_weights_ep_1000000.h5f'
+ENV_NAME = 'FlappyBird-v0'
+
+def main():
+
+    
+    env = gym.make(ENV_NAME)
+    env.seed(123)
+    nb_actions = env.action_space.n
+    input_shape= (WINDOW_LENGTH,)+(SCALED_IMAGE_HEIGHT, SCALED_IMAGE_WIDTH)
+
+    '''
+    model = Sequential()
+    model.add(Conv2D(16,(3,3),padding='same',input_shape=input_shape, data_format="channels_first"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(32,(5,5)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(4,4)))
+    #model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    #model.add(Dense(16))
+    #model.add(Activation('relu'))
+    model.add(Dense(env.action_space.n))
+    model.add(Activation('linear'))
+    '''
+
+    #network architecture
+    model = Sequential()
+    model.add(Conv2D(32,(8,8),strides=4, padding='same',input_shape=input_shape, data_format="channels_first"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(64,(4,4), strides=2))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(64,(2,2), strides=1))
+    model.add(MaxPooling2D(pool_size=(1,1)))
+    #model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    #model.add(Dense(16))
+    #model.add(Activation('relu'))
+    model.add(Dense(env.action_space.n))
+    model.add(Activation('linear'))
+
+    print(model.summary())
+    
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', 
+                                value_max=.4, value_min=.2, value_test=0.0, nb_steps=TRAINING_STEPS/5)
+    #load our processor
+    processor=GameProcessor()
+
+    #memory for experience replay
+    memory = SequentialMemory(limit=50000, window_length=WINDOW_LENGTH)
+    
+    #initialize agent
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=TRAINING_STEPS/50,
+                target_model_update=5e-2, policy=policy, processor=processor)
+    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+    if(LOAD):
+        dqn.load_weights(LOAD_FILE)
+    else:
+        dqn.fit(env, nb_steps=TRAINING_STEPS, visualize=False, verbose=2)
+
+        # After training is done, we save the final weights.
+        dqn.save_weights('dqn_{}_weights_ep_{}.h5f'.format(ENV_NAME, TRAINING_STEPS), overwrite=False)
+    
+
+    #reset processor variables before testing
+    processor.score=0
+    processor.pipes_crossed=0
+    processor.end=0
+
+    #test our agent
+    dqn.test(env, nb_episodes=TEST_EPISODES, visualize=True)
+    print('Test Score over ' + str(TEST_EPISODES) + ' episodes is : ' + str(processor.score))
+
+
 class GameProcessor(Processor):
-    #tried all other background subtractors as well
-    #tuned hyperparams
+    
+    #background subtractor
+    #tried all other background subtractors from opencv as well
+    #manually tuned hyperparams
     fgbg=cv2.bgsegm.createBackgroundSubtractorMOG(history=100, nmixtures=5, backgroundRatio=0.0000007)
+    
+    #number of total pipes crossed over all episodes. used to calculate score
     pipes_crossed=0.0
-    score=0.0
+
+    #how many times did the episode end. used for score calculation
     end=0
+
+    score=0.0
+    
     def process_observation(self, observation):
-        #print(observation.shape)
-        #observation=observation[:,:]
-        #img=Image.fromarray(observation)
-        #img=img.convert('L') #grayscale
-        #processed_observation = np.array(img)
         
-        #imgplot = plt.imshow(observation)
-        #plt.show()
-        #cv2.imshow('ob',observation)
+        #convert to grayscale
         gray_image = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
+        
+        #downscale image for more manageable training 
         processed_observation=cv2.resize(gray_image, (SCALED_IMAGE_HEIGHT, SCALED_IMAGE_WIDTH))
+        
         processed_observation=self.fgbg.apply(processed_observation)
         
-        imgplot = plt.imshow(processed_observation)
-        plt.show()
+        #kernel=(3,3)
+        #processed_observation = cv2.morphologyEx(processed_observation, cv2.MORPH_OPEN, kernel)
+
+        #uncomment to display each frame
+        #imgplot = plt.imshow(processed_observation)
+        #plt.show()
         
-        #print("observation shape=", processed_observation.shape)
-        #cv2.imshow('bgremoved',fgmask)
-        #return observation
+        #normalize pixel values [0,1]
         return np.divide(processed_observation, 255.0)
+    
+    #reward function
     def process_reward(self, reward):
         #print(reward)
         if(reward==0):
+            #we want to reward the agent by a little bit for just flying
             reward = 1.0
         elif(reward==1):
+            #reward by a lot if pipe crossed
             print('pipe_crossed')
-            reward = 25
+            reward = 100
             self.pipes_crossed+=1
-        elif(reward==-1):
-            reward = -5
         elif(reward==-5):
-            reward = -10
+            #negative reward if terminal state reached
+            reward = -20
             self.end+=1
+
+            #score calculation
             self.score=(self.pipes_crossed-self.end)/self.end
             print('pipes crossed=',self.pipes_crossed)
             print('score=', self.score)
-        #print(reward/25)
-        return reward/25
 
+            #return normalized reward [-1, 1]
+        return reward/100
 
-ENV_NAME = 'FlappyBird-v0'
-#flappyEnv=GymEnvironment('FlappyBird-v0')
-#flappyEnv.setSeed(0)
-#nb_actions=flappyEnv.getEnv().action_space
-#nb_observation_space=flappyEnv.getEnv().observation_space
-# Get the environment and extract the number of actions.
-env = gym.make(ENV_NAME)
-np.random.seed(123)
-env.seed(123)
-nb_actions = env.action_space.n
-#input_shape=(1,)+env.observation_space.shape[:2]
-input_shape= (WINDOW_LENGTH,)+(SCALED_IMAGE_HEIGHT, SCALED_IMAGE_WIDTH)
-print(input_shape)
-# Next, we build a very simple model.
-'''
-model = Sequential()
-model.add(Conv2D(16,(3,3),padding='same',input_shape=input_shape, data_format="channels_first"))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(32,(5,5)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(4,4)))
-#model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(16))
-model.add(Activation('relu'))
-#model.add(Dense(16))
-#model.add(Activation('relu'))
-model.add(Dense(env.action_space.n))
-model.add(Activation('linear'))
-'''
-model = Sequential()
-model.add(Conv2D(32,(8,8),strides=4, padding='same',input_shape=input_shape, data_format="channels_first"))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(64,(4,4), strides=2))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(64,(2,2), strides=1))
-model.add(MaxPooling2D(pool_size=(1,1)))
-#model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-#model.add(Dense(16))
-#model.add(Activation('relu'))
-model.add(Dense(env.action_space.n))
-model.add(Activation('linear'))
-
-print(model.summary())
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', 
-                              value_max=1., value_min=.1, value_test=0.0, nb_steps=TRAINING_STEPS)
-processor=GameProcessor()
-memory = SequentialMemory(limit=50000, window_length=WINDOW_LENGTH)
-#policy = BoltzmannQPolicy()
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10000,
-               target_model_update=1e-2, policy=policy, processor=processor)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-
-# Okay, now it's time to learn something! We visualize the training here for show, but this
-# slows down training quite a lot. You can always safely abort the training prematurely using
-# Ctrl + C.
-#dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
-dqn.fit(env, nb_steps=TRAINING_STEPS, visualize=False, verbose=2)
-
-# After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights_ep_{}.h5f'.format(ENV_NAME, TRAINING_STEPS), overwrite=False)
-#dqn.load_weights('dqn_FlappyBird-v0_weights_ep_1000000.h5f')
-# Finally, evaluate our algorithm for 5 episodes.
-processor.score=0
-processor.pipes_crossed=0
-processor.end=0
-dqn.test(env, nb_episodes=5, visualize=True)
-print(processor.score)
+if __name__ == "__main__":
+    main()
