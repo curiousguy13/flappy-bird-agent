8faa8a333b41dd786fa2fff67202ea4511335bfd
Kunal Arora <kunalarora.135@gmail.com>
2018-11-11 16:38:28 +0530

    Updated pleInstall.py

--- requirements/pleInstall.py
+++ requirements/pleInstall.py
@@ -1,6 +1,6 @@
 import os
-!git clone https://github.com/ntasfi/PyGame-Learning-Environment.git
+os.system("git clone https://github.com/ntasfi/PyGame-Learning-Environment.git")
 os.chdir('PyGame-Learning-Environment')
-!pip install -e .
-!pip install pygame
-os.chdir('/content')
+os.system("pip install -e .")
+os.system("pip install pygame")
+#os.chdir('/content')
